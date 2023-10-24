6cb03d63826ec59ead36b60ad408661d1dbcd48e
joshuackeller <joshuackeller@gmail.com>
2023-10-24 15:15:57 -0600

    changes

--- lua/joshuakeller/remap.lua
+++ lua/joshuakeller/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>f", ":Format<CR>")
-vim.keymap.set("n", "<leader>F", ":FormatWrite<CR>")
+vim.keymap.set("t", '<Esc>', '<C-\\><C-n>')
+vim.keymap.set("n", ":t", ":ToggleTerm<CR>")
+vim.keymap.set("n", ":T", ":ToggleTerm size=40<CR>")
