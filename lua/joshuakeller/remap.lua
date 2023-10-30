802a881a5737588f31edd2e9e283faf6723447f7
joshuackeller <joshuackeller@gmail.com>
2023-10-30 06:09:57 -0600

    diffview

--- lua/joshuakeller/remap.lua
+++ lua/joshuakeller/remap.lua
@@ -14,3 +14,12 @@ vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("n", "<leader>wd", "<C-W>c", { noremap = true, silent = true }) -- delete window
 vim.api.nvim_set_keymap("n", "<leader>wj", "<C-W>s", { noremap = true, silent = true }) -- open window right
 vim.api.nvim_set_keymap("n", "<leader>wl", "<C-W>v", { noremap = true, silent = true }) -- open window below
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Save
+vim.keymap.set("n", "<C-s>", ":w<CR>")
+vim.keymap.set("v", "<C-s>", ":w<CR>")
+
+vim.keymap.set("v", "Y", '"+y')
